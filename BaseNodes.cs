bc3c09e76ebfc43908cf4de987cf259fa0ed52c4
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-12-01 21:43:43 +0300

    add: chain end rem: until success node

--- BaseNodes.cs
+++ BaseNodes.cs
@@ -13,37 +13,6 @@ namespace Strategies
         public abstract void Execute(IEntity entity);
     }
 
-    /// <summary>
-    /// общая логика - лочим стратегию на этой ноде, проверяем условие по которому мы считаем что логика успешно завершена, 
-    /// и вызываем метод Success
-    /// в противном случае дергаем ноду proccess, подразумевается что мы там что то делаем что в итоге приведет к успеху
-    /// </summary>
-    public abstract class UntilSuccesNode : BaseDecisionNode
-    {
-        [Connection(ConnectionPointType.In, "Input")] public BaseDecisionNode parent;
-        [Connection(ConnectionPointType.Link, "When Success")] public BaseDecisionNode whenSuccess;
-        [Connection(ConnectionPointType.Link, "Proccess")] public BaseDecisionNode proccess;
-
-        public override void Execute(IEntity entity)
-        {
-            if (entity.ContainsMask<UntilSuccessStrategyNodeComponent>())
-                LocalExecute(entity);
-            else
-            {
-                entity.AddHecsComponent(new UntilSuccessStrategyNodeComponent { BaseDecisionNode = this });
-                LocalExecute(entity);
-            }
-        }
-
-        protected void Success(IEntity entity)
-        {
-            entity.RemoveHecsComponent<UntilSuccessStrategyNodeComponent>();
-            whenSuccess.Execute(entity);
-        }
-
-        public abstract void LocalExecute(IEntity entity);
-    }
-
     public abstract class InterDecision : BaseDecisionNode
     {
         [Connection(ConnectionPointType.In, "Input")] [IgnoreDraw] public BaseDecisionNode parent;
---
+++ ChainEnd.cs
@@ -0,0 +1,18 @@
+﻿using HECSFramework.Core;
+using HECSFramework.Documentation;
+
+namespace Strategies
+{
+    [Documentation(Doc.AI, Doc.Strategy, "Это нода пустышка которую мы ставим в конце цепочки для того чтобы не проверять на налл каждую следующую ноду  ")]
+    public class ChainEnd : StateNode
+    {
+        [Connection(ConnectionPointType.In, "Input")]
+        public BaseDecisionNode inputNode;
+
+        public override string TitleOfNode => "Chain End";
+
+        protected override void ExecuteState(IEntity entity)
+        {
+        }
+    }
+}
