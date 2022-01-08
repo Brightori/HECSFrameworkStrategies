6a61942c4ae6f7888619d50e02007c38e16be87c
Eugen Dubovik <e.dubovik@kefirgames.com>
2022-01-08 03:15:15 +0300

    upd: node documentation

--- CompleteFinalNode.cs
+++ CompleteFinalNode.cs
@@ -3,6 +3,7 @@ using HECSFramework.Core;
 
 namespace Strategies
 {
+    [Documentation(Doc.Strategy, "Эта нода завершения стратегии, она посылает команду перезапуска стратегии, если нужно чтобы стратегия не перезапускалась - нужна другая нода")]
     public class CompleteFinalNode : FinalDecision
     {
         public override string TitleOfNode => "Strategy Complete";
