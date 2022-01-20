e21f29b7dee9984f3e9d404bc4d6b972d5a54671
Eugen Dubovik <e.dubovik@kefirgames.com>
2022-01-20 13:10:32 +0300

    add: documentation to need decision command

--- Commands/NeedDecisionCommand.cs
+++ Commands/NeedDecisionCommand.cs
@@ -1,7 +1,10 @@
 using HECSFramework.Core;
+using Strategies;
+using Systems;
 
 namespace Commands
 {
+	[Documentation(Doc.AI, Doc.Strategy, "Эта команда означает что пора перезапустить стратегию и пройти всю логическую цепочку заново, отправляется обычно из " + nameof(CompleteFinalNode) + " обрабатывается обычно в " + nameof(AINPCSystem))]
 	public struct NeedDecisionCommand : ICommand
 	{
 	}
