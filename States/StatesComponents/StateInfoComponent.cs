c165b9d40e2759502dd7f8f4572f53fd06bf8d21
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-02-23 15:27:45 +0300

    add: dispose to stateInfoComponent

--- States/StatesComponents/StateInfoComponent.cs
+++ States/StatesComponents/StateInfoComponent.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 namespace Components
 {
     [Serializable, Documentation(Doc.AI, Doc.Strategy, Doc.State, Doc.Debug, "Компонент который вешается на ентити которая участвует в данном стейте, нужен для дебага")]
-    public partial class StateInfoComponent : BaseComponent
+    public partial class StateInfoComponent : BaseComponent, IDisposable
     {
         public bool NeedInfo = false;
         public bool NeedClean = false;
@@ -15,7 +15,14 @@ namespace Components
         public int MaxCycles = 3;
         public int CurrentCycle = 0;
 
+#if UNITY_EDITOR
         [OdinSerialize]
+#endif
         public List<LogNode> StateStack = new List<LogNode>();
+
+        public void Dispose()
+        {
+            StateStack.Clear();
+        }
     }
 }
