7ee8c9ebc1b4ceed38a2bafdc6121a0c46560ed7
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-03-14 09:45:21 +0300

    upd: now strategies service is a part of world. not game controller

---
+++ WorldStrategiesPart.cs
@@ -0,0 +1,15 @@
+﻿using Systems;
+
+namespace HECSFramework.Core
+{
+    public partial class World
+    {
+        partial void AddStrategiesPart()
+        {
+            var strategiesController = GetEntityFromPool("Strategies Controller");
+            strategiesController.AddHecsSystem(new StrategiesMainServiceSystem());
+            strategiesController.AddHecsSystem(new StateUpdateSystem());
+            strategiesController.Init();
+        }
+    }
+}
