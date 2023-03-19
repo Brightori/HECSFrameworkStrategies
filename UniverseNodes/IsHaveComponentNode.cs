5b64300876c56f989c67b9e3118fbeacd0307062
Brightori <el.brightori@gmail.com>
2023-03-20 02:22:37 +0300

    fix: ishaveComponents node, now have good behaviour on default component

--- UniverseNodes/IsHaveComponentNode.cs
+++ UniverseNodes/IsHaveComponentNode.cs
@@ -11,12 +11,12 @@ namespace Strategies
         public override string TitleOfNode { get; } = "Is Have Component";
 
         [ComponentMaskDropDown]
-        [Field(0, typeof(HECSMaskResolver))]
-        public HECSMask ComponentMask;
+        [Field(0)]
+        public int ComponentMask;
 
         protected override void Run(Entity entity)
         {
-            if (entity.ContainsMask(ComponentMask.TypeHashCode))
+            if (entity.ContainsMask(ComponentMask))
                 Positive.Execute(entity);
             else
                 Negative.Execute(entity);
@@ -26,12 +26,8 @@ namespace Strategies
         //mby this component not available anymore
         public void Init()
         {
-            if (TypesMap.GetComponentInfo(ComponentMask.TypeHashCode, out var mask))
-            {
-                ComponentMask = mask.ComponentsMask;
-            }
-            else
-                HECSDebug.LogError("is have component node contains wrong mask " + ComponentMask.TypeHashCode);
+            if (!TypesMap.ContainsComponent(ComponentMask))
+                HECSDebug.LogError("is have component node contains wrong mask " + ComponentMask);
         }
     }
 }
