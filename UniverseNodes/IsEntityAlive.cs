1871b7e223ba119b90e36f949af74c05597707c8
Brightori <el.brightori@gmail.com>
2023-08-14 00:49:40 +0300

    upd: is alive entity node now have optional entity

--- UniverseNodes/IsEntityAlive.cs
+++ UniverseNodes/IsEntityAlive.cs
@@ -6,10 +6,27 @@ namespace Strategies
     [Documentation(Doc.HECS, Doc.UniversalNodes, Doc.Strategy, "this node return is entity alive or not")]
     public class IsEntityAlive : DilemmaDecision
     {
+        [Connection(ConnectionPointType.In, "<Entity> optional entity")]
+        public GenericNode<Entity> OptionalEntity;
+
         public override string TitleOfNode { get; } = "IsEntityAlive";
 
         protected override void Run(Entity entity)
         {
+            if (OptionalEntity != null) 
+            {
+                if (OptionalEntity.Value(entity).IsAlive())
+                {
+                    Positive.Execute(entity);
+                    return;
+                }
+                else
+                {
+                    Negative.Execute(entity);
+                    return;
+                }
+            }
+
             if (entity.IsAlive())
             {
                 Positive.Execute(entity);
