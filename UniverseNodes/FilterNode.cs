37699cd1ca8f738f285be9e4bda84b6f792e5f15
Brightori <el.brightori@gmail.com>
2023-08-13 14:49:12 +0300

    add: filter node

---
+++ UniverseNodes/FilterNode.cs
@@ -0,0 +1,29 @@
+﻿using HECSFramework.Core;
+
+namespace Strategies
+{
+    [Documentation(Doc.UniversalNodes, Doc.HECS, Doc.Strategy, "this node provides filters for operations")]
+    public sealed class FilterNode : GenericNode<FilterNode>
+    {
+        [DrawEntitiesFilter]
+        public Filter Include;
+
+        [DrawEntitiesFilter]
+        public Filter Exclude;
+
+        [Connection(ConnectionPointType.Out, "<FilterNode> Out")]
+        public BaseDecisionNode Out;
+
+        public override string TitleOfNode { get; } = "FilterNode";
+
+        public override void Execute(Entity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override FilterNode Value(Entity entity)
+        {
+            return this;
+        }
+    }
+}
