5406c7b9428c91ff84e4ca3cf7cf59571718b0d3
Brightori <el.brightori@gmail.com>
2023-09-15 00:02:14 +0300

    add: empty final node

---
+++ UniverseNodes/EmptyFinalNode.cs
@@ -0,0 +1,17 @@
+using HECSFramework.Core;
+
+namespace Strategies
+{
+    [Documentation(Doc.Strategy, Doc.HECS, "Empty node whithout logic for finaling chains|strategis")]
+    public sealed class EmptyFinalNode : BaseDecisionNode
+    {
+        [Connection(ConnectionPointType.In, "Input")]
+        public BaseDecisionNode Input;
+
+        public override string TitleOfNode { get; } = "EmptyFinalNode";
+
+        public override void Execute(Entity entity)
+        {
+        }
+    }
+}
