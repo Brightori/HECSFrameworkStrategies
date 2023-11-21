d76be4330a47a255f0c669d3448d9238db41e286
Brightori <el.brightori@gmail.com>
2023-11-21 15:16:50 +0300

    fix: get or add counter node miss next node execution

--- UniverseNodes/GetOrAddFloatCounterNode.cs
+++ UniverseNodes/GetOrAddFloatCounterNode.cs
@@ -19,6 +19,8 @@ namespace Strategies
             {
                 holder.AddCounter(new DefaultFloatCounter { Id = CounterID });
             }
+
+            Next.Execute(entity);
         }
     }
 }
