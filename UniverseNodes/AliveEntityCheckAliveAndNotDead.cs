1363a8e040de5c9e5db7992f2130ffbe60983b3e
Brightori <el.brightori@gmail.com>
2024-03-06 01:27:57 +0300

    add: alive entity check alive and not dead node

---
+++ UniverseNodes/AliveEntityCheckAliveAndNotDead.cs
@@ -0,0 +1,25 @@
+﻿using HECSFramework.Core;
+
+namespace Strategies
+{
+    [Documentation(Doc.Strategy, Doc.HECS, "AliveEntityCheckAlive")]
+    public class AliveEntityCheckAliveAndNotDead : DilemmaDecision
+    {
+        [Connection(ConnectionPointType.In, "<AliveEntity> AliveEntity")]
+        public GenericNode<AliveEntity> AliveEntity;
+        public override string TitleOfNode { get; } = "AliveEntityCheckAlive";
+        protected override void Run(Entity entity)
+        {
+            if (AliveEntity.Value(entity).IsAliveAndNotDead())
+            {
+                Positive.Execute(entity);
+                return;
+            }
+            else
+            {
+                Negative.Execute(entity);
+                return;
+            }
+        }
+    }
+}
