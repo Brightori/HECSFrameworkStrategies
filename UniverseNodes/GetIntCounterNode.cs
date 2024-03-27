cbc8d9e7883d65a4a7e6e395968a42ac5c6c2f51
Brightori <el.brightori@gmail.com>
2024-03-28 02:40:56 +0300

    add: get int counter node

---
+++ UniverseNodes/GetIntCounterNode.cs
@@ -0,0 +1,30 @@
+﻿using Components;
+using HECSFramework.Core;
+
+namespace Strategies
+{
+    [Documentation(Doc.Strategy, "GetIntCounterNode")]
+    public sealed partial class GetIntCounterNode : GenericNode<int>
+    {
+        public override string TitleOfNode { get; } = "GetIntCounterNode";
+
+        [Connection(ConnectionPointType.In, " <Entity> Additional Entity")]
+        public GenericNode<Entity> AdditionalEntity;
+
+        [Connection(ConnectionPointType.Out, " <int> Out")]
+        public BaseDecisionNode Out;
+
+        [DropDownIdentifier("CounterIdentifierContainer")]
+        public int CounterIdentifier;
+
+        public override void Execute(Entity entity)
+        {
+        }
+
+        public override int Value(Entity entity)
+        {
+            var neededEntity = AdditionalEntity != null ? AdditionalEntity.Value(entity) : entity;
+            return neededEntity.GetComponent<CountersHolderComponent>().GetCounter<ICounter<int>>(CounterIdentifier).Value;
+        }
+    }
+}
